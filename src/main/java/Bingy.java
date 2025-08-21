49351457db890e3ca64ea1d3ac2b341051c273b1
Bing Hang <binghangc@gmail.com>
2025-08-21 20:07:11 +0800

    Ability to mark tasks as done or not
    Previously, taskList is just a string array. To fully support boolean
    isDone for individual tasks, Task class is added and is encapsulated
    within taskList.

--- src/main/java/Bingy.java
+++ src/main/java/Bingy.java
@@ -12,17 +12,25 @@ public class Bingy {
 
 class TaskManager {
     private static final String line = "____________________________________________________________";
-    private List<String> tasks;
+    private List<Task> tasks;
 
     public TaskManager(int size) {
         tasks = new ArrayList<>();
     }
 
-    public void addTask(String task) {
-        tasks.add(task);
+    public void markDone(int index) {
+        tasks.get(index).markDone();
     }
 
-    public List<String> getTasks() {
+    public void markUndone(int index) {
+        tasks.get(index).markUndone();
+    }
+
+    public void addTask(String task) {
+        tasks.add(new Task(task));
+    }
+
+    public List<Task> getTasks() {
         return Collections.unmodifiableList(tasks);
     }
 
@@ -58,27 +66,77 @@ class BingyBot {
         System.out.println(logo);
         System.out.println(line);
         System.out.println(" Boo! I'm Bingy");
-        System.out.println(" WHAT caan I doooOoo for yoou?");
+        System.out.println(" WHAT caan't I doooOoo for yoou?");
         System.out.println(line);
     }
 
     private void handleInput(String input) {
-        if (input.trim().equalsIgnoreCase("bye")) {
+        String trimmed = input.trim();
+
+
+        if (trimmed.equalsIgnoreCase("bye")) {
             sayGoodbye();
             running = false;
-        } else if (input.trim().equalsIgnoreCase("list")) {
+            return;
+        }
+
+
+        if (trimmed.equalsIgnoreCase("list")) {
             System.out.println(line);
-            List<String> tasks = taskManager.getTasks();
+            System.out.println("Here's the list of chores you will NOT complete MUAHAHAH");
+            List<Task> tasks = taskManager.getTasks();
             for (int i = 0; i < tasks.size(); i++) {
-                System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
+                System.out.println(String.format("%d. %s", i + 1, tasks.get(i).toString()));
             }
             System.out.println(line);
-        } else {
-            taskManager.addTask(input);
-            System.out.println(line);
-            System.out.println("added: " + input);
-            System.out.println(line);
+            return;
         }
+
+
+        String[] parts = trimmed.split("\\s+");
+        String command = parts[0];
+
+
+        if (command.equalsIgnoreCase("mark") || command.equalsIgnoreCase("unmark")) {
+            if (parts.length < 2) {
+                System.out.println(line);
+                System.out.println("What the helly. Give me a task number like \"Mark 2\" or something. Quick");
+                System.out.println(line);
+                return;
+            }
+            int taskIndex;
+            try {
+                taskIndex = Integer.parseInt(parts[1]) - 1; // to 0-based
+            } catch (NumberFormatException e) {
+                System.out.println(line);
+                System.out.println(" Give me a task number. Numbers are characters like 1 or 2. Hope that helps!");
+                System.out.println(line);
+                return;
+            }
+            List<Task> tasks = taskManager.getTasks();
+
+            Task t = tasks.get(taskIndex);
+            if (command.equalsIgnoreCase("mark")) {
+                taskManager.markDone(taskIndex);
+                System.out.println(line);
+                System.out.println(" Hopefully you did the task properly... Marked it for you");
+                System.out.println("   " + t);
+                System.out.println(line);
+            } else {
+                taskManager.markUndone(taskIndex);
+                System.out.println(line);
+                System.out.println(" Ha! I knew you couldn't do it. Unmarked it! Welcome");
+                System.out.println("   " + t);
+                System.out.println(line);
+            }
+            return;
+        }
+
+
+        taskManager.addTask(trimmed);
+        System.out.println(line);
+        System.out.println("added: " + trimmed);
+        System.out.println(line);
     }
 
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private boolean isDone;
+    private final String description;
+
+    public Task (String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUndone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.getDescription());
+    }
+}
