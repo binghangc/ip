43aa9622f830d91fb2a69806a69065d6e08ac4f8
Bing Hang <binghangc@gmail.com>
2025-08-21 11:13:44 +0800

    Rename main class and implement skeletal version
    The chatbot was previously using the placeholder name Duke.
    
    Renamed the main class to Bingy to differentiate from other chatbots.
    Add a greeting and exit message to establish a basic skeletal structure.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bingy User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bingy.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+public class Bingy {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String logo =
+                ".-. .-')                .-') _                         \n"
+                        + "\\  ( OO )              ( OO ) )                        \n"
+                        + " ;-----.\\   ,-.-') ,--./ ,--,'  ,----.      ,--.   ,--.\n"
+                        + " | .-.  |   |  |OO)|   \\ |  |\\ '  .-./-')    \\  `.'  / \n"
+                        + " | '-' /_)  |  |  \\|    \\|  | )|  |_( O- ) .-')     /  \n"
+                        + " | .-. `.   |  |(_/|  .     |/ |  | .--, \\\\(OO  \\   /   \n"
+                        + " | |  \\  | ,|  |_.'|  |\\    | (|  | '. (_/ |   /  /\\_  \n"
+                        + " | '--'  /(_|  |   |  | \\   |  |  '--'  |  `-./  /.__) \n"
+                        + " `------'   `--'   `--'  `--'   `------'     `--'      \n";
+
+        System.out.println(logo);
+        System.out.println(line + "\n Boo! I'm Bingy \n");
+        System.out.println(" What can I do for you? \n");
+        System.out.println(line + "\n Bye. Hope to see you again soon! \n" + line);
+    }
+}
