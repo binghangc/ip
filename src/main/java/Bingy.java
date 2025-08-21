415ea70120016f57d9da1c6049906e798b3d8e22
Bing Hang <binghangc@gmail.com>
2025-08-21 16:39:09 +0800

    Refactor chatbot into BingyBot class
    Implementation previously contained the entire code within the main
    class.
    
    Separation of concerns help improve readability and makes it easier to
    maintain code and add features in future

--- src/main/java/Bingy.java
+++ src/main/java/Bingy.java
@@ -2,28 +2,61 @@ import java.util.Scanner;
 
 public class Bingy {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String logo =
-                ".-. .-')                .-') _                         \n"
-                        + "\\  ( OO )              ( OO ) )                        \n"
-                        + " ;-----.\\   ,-.-') ,--./ ,--,'  ,----.      ,--.   ,--.\n"
-                        + " | .-.  |   |  |OO)|   \\ |  |\\ '  .-./-')    \\  `.'  / \n"
-                        + " | '-' /_)  |  |  \\|    \\|  | )|  |_( O- ) .-')     /  \n"
-                        + " | .-. `.   |  |(_/|  .     |/ |  | .--, \\\\(OO  \\   /   \n"
-                        + " | |  \\  | ,|  |_.'|  |\\    | (|  | '. (_/ |   /  /\\_  \n"
-                        + " | '--'  /(_|  |   |  | \\   |  |  '--'  |  `-./  /.__) \n"
-                        + " `------'   `--'   `--'  `--'   `------'     `--'      \n";
-        Scanner res = new Scanner(System.in);
-
-        System.out.println(logo);
-        System.out.println(line + "\n Boo! I'm Bingy \n");
-        System.out.println(" WHAT caan I doooOoo for yoou? \n");
-        String response = res.nextLine();
-        while (!response.trim().equalsIgnoreCase("bye")) {
-            System.out.println(line + "\n" + response + "\n" + line);
-            response = res.nextLine();
-        }
-        System.out.println(line + "\n bbbbYEE. hope to scareee you again soooooOOon! \n" + line);
-        res.close();
+        BingyBot bot = new BingyBot();
+        bot.run();
     }
+}
+
+class BingyBot {
+    private static final String line = "____________________________________________________________";
+    private static final String logo =
+            ".-. .-')                .-') _                         \n"
+          + "\\  ( OO )              ( OO ) )                        \n"
+          + " ;-----.\\   ,-.-') ,--./ ,--,'  ,----.      ,--.   ,--.\n"
+          + " | .-.  |   |  |OO)|   \\ |  |\\ '  .-./-')    \\  `.'  / \n"
+          + " | '-' /_)  |  |  \\|    \\|  | )|  |_( O- ) .-')     /  \n"
+          + " | .-. `.   |  |(_/|  .     |/ |  | .--, \\\\(OO  \\   /   \n"
+          + " | |  \\  | ,|  |_.'|  |\\    | (|  | '. (_/ |   /  /\\_  \n"
+          + " | '--'  /(_|  |   |  | \\   |  |  '--'  |  `-./  /.__) \n"
+          + " `------'   `--'   `--'  `--'   `------'     `--'      \n";
+    private boolean running = true;
+
+
+    public void run() {
+        greet();
+        Scanner sc = new Scanner(System.in);
+        while (running) {
+            String input = sc.nextLine();
+            handleInput(input);
+        }
+        sc.close();
+    }
+
+    private void greet() {
+        System.out.println(logo);
+        System.out.println(line);
+        System.out.println(" Boo! I'm Bingy");
+        System.out.println(" WHAT caan I doooOoo for yoou?");
+        System.out.println(line);
+    }
+
+    private void handleInput(String input) {
+        if (input.trim().equalsIgnoreCase("bye")) {
+            sayGoodbye();
+            running = false;
+        } else {
+            echo(input);
+        }
+    }
+
+
+    private void echo(String input) {
+        System.out.println(line + "\n " + input + "\n" + line);
+    }
+
+    private void sayGoodbye() {
+        System.out.println(line + "\n bbbbYEE. hope to scareee you again soooooOOon! \n" + line);
+    }
+
+
 }
